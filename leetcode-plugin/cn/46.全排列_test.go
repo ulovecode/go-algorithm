0f7cdd82ea947714f1f472ab41f5bd761f08076a
jackzhu <ulovecode@gmail.com>
2019-08-15 16:15:22 +0800

    【修改】修正测试方法

--- leetcode-plugin/cn/46.全排列_test.go
+++ leetcode-plugin/cn/46.全排列_test.go
@@ -1,7 +1,9 @@
 package cn
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -22,8 +24,19 @@ func Test_permute(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := permute(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("permute() = %v,\n want %v", got, tt.want)
+			got := permute(tt.args.nums)
+			strGot := make([]string, 0)
+			for _, v := range got {
+				strGot = append(strGot, fmt.Sprint(v))
+			}
+			strWant := make([]string, 0)
+			for _, v := range tt.want {
+				strWant = append(strWant, fmt.Sprint(v))
+			}
+			sort.Strings(strGot)
+			sort.Strings(strWant)
+			if !reflect.DeepEqual(strGot, strWant) {
+				t.Errorf("permute() = %v,\n want %v", strGot, strWant)
 			}
 		})
 	}
